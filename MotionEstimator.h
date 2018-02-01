c695cf22a880af2ea60363bbb89f15bd1c2301e1
Prakriti C <c.prakriti@gmail.com>
2018-02-01 15:25:49 -0800

    Update MotionEstimator.h

--- MotionEstimator.h
+++ MotionEstimator.h
@@ -1,6 +1,6 @@
-/*
- * Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
-*/
+//
+// Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
+//
 
 #ifndef MotionEstimator_H
 #define MotionEstimator_H
